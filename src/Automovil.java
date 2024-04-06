2feb0a2ca2abdea0368a9569ca53d3ddb4988167
Mariano Daniel Gobea Alcoba <mariano.gobea@mercadolibre.com>
2024-04-06 16:50:16 -0300

    Getters & Setters, constructores, sobrecarga de métodos, estaticos y ocultamiento

--- src/Automovil.java
+++ src/Automovil.java
@@ -1,15 +1,190 @@
 public class Automovil {
 
-    // Atributos de la clase Automovil. Se declaran como publicos para poder acceder a ellos desde otra clase
-    String fabricante = "Ford";
-    String modelo = "Fiesta";
-    String color = "Gris"; // Valor por defecto que puede ser modificado
-    double cilindrada = 1.6; // Valor por defecto que puede ser modificado
+    // Atributos de las instancias de la clase Automóvil. Se declaran como públicos para poder acceder a ellos desde otra clase
+    private int id; // Atributo de instancia. Es redundante asignar por defecto el valor 0 ya que es el valor por defecto de los enteros
+    private String fabricante = "Ford"; // private indica que el atributo es privado y solo se puede acceder a él desde la misma clase
+    private String modelo = "Ka";
+    private String color = "Gris"; // Valor por defecto que puede ser modificado
+    private double cilindrada = 1.6; // Valor por defecto que puede ser modificado
+    private int capacidadTanque = 32; // Valor por defecto que puede ser modificado
 
-    // Los atributos o métodos estáticos pertenecen a la clase y no a una instancia de la clase. Se accede a ellos mediante el nombre de la clase
+    // Atributos de clase o atributos estáticos
+    private static int cantidadRuedas = 4; // Se puede acceder a este atributo sin necesidad de crear una instancia de la clase Automóvil
+    private static String colorPatente = "Naranja"; // Se puede acceder a este atributo sin necesidad de crear una instancia de la clase Automóvil
+    private static int ultimoId; // Se puede acceder a este atributo sin necesidad de crear una instancia de la clase Automóvil
+
+    // 1° Sección de métodos: Constructores
+    // Creo el constructor de la clase Automóvil
+    // Puedo tener sobre carga de constructores por si necesito crear objetos de la clase Automóvil con diferentes atributos o con ninguno
+    public Automovil() {
+        // Constructor vacío
+        this.id = ++ultimoId; // Incremento el valor de la variable últimoId y se lo asigno al atributo id. Si es el primer objeto creado, id valdrá 1
+    }
+
+    public Automovil(String fabricante, String modelo) {
+        // El constructor es un método especial que se llama al crear una instancia de la clase
+        // Se puede utilizar para inicializar los atributos de la clase
+        this(); // Llamo al constructor sin parámetros
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+    }
+
+    public Automovil(String fabricante, String modelo, String color) {
+        this(fabricante, modelo); // Llamo al constructor con dos parámetros
+        this.color = color;
+    }
+
+    public Automovil(String fabricante, String modelo, String color, double cilindrada) {
+        this(fabricante, modelo, color); // Llamo al constructor con tres parámetros
+        this.cilindrada = cilindrada;
+    }
+
+    public Automovil(String fabricante, String modelo, String color, double cilindrada, int capacidadTanque) {
+        this(fabricante, modelo, color, cilindrada); // Llamo al constructor con cuatro parámetros
+        this.capacidadTanque = capacidadTanque;
+    }
+
+    // 2° Sección de métodos: Getters y Setters
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFabricante() {
+        return this.fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getCilindrada() {
+        return this.cilindrada;
+    }
+
+    public void setCilindrada(double cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public int getCapacidadTanque() {
+        return this.capacidadTanque;
+    }
+
+    public void setCapacidadTanque(int capacidadTanque) {
+        this.capacidadTanque = capacidadTanque;
+    }
+
+    // Getters & Setters de los atributos de clase o atributos estáticos
+    public static int getCantidadRuedas() {
+        return cantidadRuedas;
+    }
+
+    public static void setCantidadRuedas(int cantidadRuedas) {
+        Automovil.cantidadRuedas = cantidadRuedas; // Se accede a un atributo estático con el nombre de la clase
+    }
+
+    public static String getColorPatente() {
+        return colorPatente;
+    }
+
+    public static void setColorPatente(String colorPatente) {
+        Automovil.colorPatente = colorPatente; // Se accede a un atributo estático con el nombre de la clase
+    }
+
+    public static int getUltimoId() {
+        return ultimoId;
+    }
+
+    public static void setUltimoId(int ultimoId) {
+        Automovil.ultimoId = ultimoId; // Se accede a un atributo estático con el nombre de la clase
+    }
+
+    // En un método estético no se puede acceder a atributos de instancia, solo a atributos de clase. También a valores que se pase por parámetro.
+
+    // 3° sección de métodos: Métodos de la clase Automóvil que usan sus objetos
     public void imprimirDetalle() {
         System.out.println("Mi auto es un " + this.fabricante + " " + this.modelo + " de color " + this.color + " y cilindrada " + this.cilindrada + " litros.");
         // "this" es una referencia al objeto actual. Se utiliza para acceder a los atributos y métodos de la clase. Se podría omitir en este caso. Pero es una buena práctica utilizarlo.
+        // Dentro de una clase que representa datos no es buena práctica utilizar System.out.println. Se debe separar la lógica de la presentación. Esto iría en una clase main.
     }
 
+    public String retornarDetalle() {
+        return "Mi auto es un " + this.getFabricante() + " " + this.getModelo() + " de color " + this.getColor() + " y cilindrada " + this.getCilindrada() + " litros.";
+    }
+
+    public String acelerar(int rpm) {
+        return "El auto " + this.fabricante + " " + this.modelo + " está acelerando a " + rpm + " rpm.";
+    }
+
+    public String frenar() {
+        return "El auto " + this.fabricante + " " + this.modelo + " está frenando.";
+    }
+
+    public String acelerarFrenar(int rpm) {
+        String aceleracion = this.acelerar(rpm);
+        String frenado = this.frenar();
+        return aceleracion + "\n" + frenado;
+    }
+
+    public double calcularConsumo(double kms, double porcentajeCombustible) {
+        return kms / (this.capacidadTanque * porcentajeCombustible);
+    }
+
+    public double calcularConsumo(double kms, int porcentajeCombustible) {
+        return kms / (this.capacidadTanque * (porcentajeCombustible / 100f));
+    } // Se sobrecarga el método calcularConsumo para recibir un porcentaje como entero. Se convierte a float para que la división sea decimal
+
+    // 4° Sección de métodos: Sobreescribir métodos de la clase o superclase
+    // Sobreescribo el método equals de la clase Object para comparar dos objetos de la clase Automóvil
+    @Override
+    public boolean equals(Object obj) {
+        // Se verifica que el objeto de comparación no sea el mismo objeto
+        if (this == obj) {
+            return true;
+        }
+        // Se verifica que el objeto sea de la clase Automóvil
+        if (!(obj instanceof Automovil)) {
+            return false;
+        }
+        // Se castea el objeto a la clase Automóvil y se comparan los atributos
+        Automovil auto = (Automovil) obj;
+        return (this.getFabricante() != null
+                && this.getModelo() != null
+                && this.getFabricante().equals(auto.getFabricante())
+                && this.getModelo().equals(auto.getModelo())
+                && this.getColor().equals(auto.getColor()));
+    }
+
+    // Sobreescribo el método toString de la clase Object para devolver una representación en String del objeto
+    @Override
+    public String toString() {
+        return "Automovil [fabricante=" + this.fabricante + // Los atributos de objeto se acceden con this
+                ", modelo=" + this.modelo +
+                ", color=" + this.getColor() + // Los métodos de objeto se acceden con this
+                ", cilindrada=" + this.getCilindrada() +
+                ", capacidadTanque=" + this.capacidadTanque +
+                ", cantidadRuedas=" + Automovil.cantidadRuedas + // Los atributos de clase se acceden con el nombre de la clase
+                ", colorPatente=" + Automovil.colorPatente +
+                ", id=" + this.id +
+                "]";
+    }
 }
--- src/EjemploAutomovil.java
+++ src/EjemploAutomovil.java
@@ -1,33 +1,88 @@
+import java.util.Date;
+
 public class EjemploAutomovil {
     public static void main(String[] args) {
-        Automovil toyota = new Automovil(); // El new crea una instancia de la clase Automovil
+        Automovil toyota = new Automovil("Toyota", "Corolla");
 
-        // Seteo los atributos que son publicos y puedo acceder a ellos. Mala práctica. Se deben proteger y leer o modificar mediante métodos (getters y setters)
-        toyota.fabricante = "Toyota";
-        toyota.modelo = "Corolla";
-        toyota.color = "Rojo";
-        toyota.cilindrada = 1.8;
-
-        // Creo un nuevo objeto de la clase Automovil
-        Automovil ford = new Automovil();
+        // Uso los métodos setters para asignar valores a los atributos de la clase Automovil
+        toyota.setColor("Blanco");
+        toyota.setCilindrada(1.6);
+        toyota.setCapacidadTanque(50);
 
         // Creo un nuevo objeto de la clase Automovil
-        Automovil chevrolet = new Automovil();
+        Automovil ford = new Automovil("Ford", "Fiesta");
+
+        // Creo un nuevo objeto de la clase Automovil
+        Automovil chevrolet = new Automovil("Chevrolet", "Corsa");
 
         // Seteo los atributos que son publicos y puedo acceder a ellos. Mala práctica. Se deben proteger y leer o modificar mediante métodos (getters y setters)
-        chevrolet.fabricante = "Chevrolet";
-        chevrolet.modelo = "Corsa";
-        chevrolet.color = "Azul";
-        chevrolet.cilindrada = 1.4;
+        chevrolet.setColor("Gris");
+        chevrolet.setCilindrada(1.4);
+        chevrolet.setCapacidadTanque(45);
 
-        Automovil[] autos = new Automovil[3];
+        // Creo un nuevo automovil nissan
+        Automovil nissan = new Automovil("Nissan", "Sentra", "Rojo", 1.8, 60);
+        Automovil nissan2 = new Automovil("Nissan", "Sentra", "Rojo", 1.8, 60);
+        Automovil autoDefault = new Automovil();
+
+        // Creo un objeto de tipo Date para comparar con el nissan
+        Date fecha = new Date();
+
+        // Modifico el color de la patente de la clase
+        Automovil.setColorPatente("Amarillo");
+
+        System.out.println();
+        System.out.println("nissan == nissan2: " + (nissan == nissan2));
+        System.out.println("nissan.equals(nissan2): " + nissan.equals(nissan2)); // Hace lo mismo que el == salvo que se puede sobreescribir
+        // Sin sobreescritura de equals() en la clase Automovil, el resultado es el mismo que el ==. Si se sobreescribe, se puede comparar por los atributos de la clase
+        System.out.println("nissan.equals(autoDefault): " + nissan.equals(autoDefault)); // false
+        System.out.println("nissan.equals(fecha): " + nissan.equals(fecha)); // false
+        // También puede pasar que comparé el objeto con si mismo
+        System.out.println("nissan.equals(nissan): " + nissan.equals(nissan)); // true
+        System.out.println();
+
+        // Sección de toString()
+        System.out.println("toyota.toString(): " + toyota.toString()); // Imprime la dirección de memoria del objeto dado que no se ha sobreescrito el método toString() Automovil@4f023edb
+        // Con el metodo sobreescrito se imprime el detalle del objeto: Automovil [fabricante=Toyota, modelo=Corolla, color=Blanco, cilindrada=1.6, capacidadTanque=50]
+        System.out.println();
+
+        Automovil[] autos = new Automovil[4];
         autos[0] = toyota;
         autos[1] = ford;
         autos[2] = chevrolet;
+        autos[3] = nissan;
 
         for (Automovil auto : autos) {
-            auto.imprimirDetalle();
+            System.out.println(auto); // Aplica el método toString() de la clase Automovil
+        }
+
+        System.out.println();
+
+        for (Automovil auto : autos) {
+            String detalle = auto.retornarDetalle();
+            System.out.println(detalle);
         } // Imprime los detalles de cada auto
 
+        // Acelerar el auto Toyota
+        System.out.println(toyota.acelerar(3000));
+
+        // Frenar el auto Toyota
+        System.out.println(toyota.frenar());
+
+        // Acelerar y frenar el auto ford;
+        System.out.println(ford.acelerarFrenar(2000));
+
+        // Calcular el consumo del auto Chevrolet
+        double consumo = chevrolet.calcularConsumo(100, 0.5);
+        System.out.println("El consumo del auto Chevrolet es de " + consumo + " litros cada 100 kms.");
+
+        // Calcular el consumo del auto Toyota con el porcentaje de combustible como entero
+        consumo = toyota.calcularConsumo(100, 50);
+        System.out.println("El consumo del auto Toyota es de " + consumo + " litros cada 100 kms.");
+
+        // Imprimo el fabricante de cada auto
+        for (Automovil auto : autos) {
+            System.out.println("Fabricante: " + auto.getFabricante());
+        }
     }
 }
